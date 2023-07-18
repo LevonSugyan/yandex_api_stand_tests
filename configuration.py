075b3f709d9a7b9aab88e9fa337928bc4759403a
Levon Sugyan <ls@hlebnaya-usadba.ru>
2023-07-18 12:37:03 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://9d1817b7-e73b-435c-833e-fbf3f51dadc5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
